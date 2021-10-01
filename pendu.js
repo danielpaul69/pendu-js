f1cc7fce18f16aff308882dd7dcdbbbcab7fe86e
Daniel Paul <daniel.paul@codgency.fr>
2021-10-01 12:25:05 +0200

    Etape 2

--- pendu.js
+++ pendu.js
@@ -5,8 +5,35 @@ let animaux = [
     'canard'
 ];
 
+let lettresSaisies = [];
+
 let motAleatoire = animaux[Math.floor(Math.random() * animaux.length)];
+console.log(motAleatoire);
+
 let espaceJeu = document.getElementById('espaceJeu');
-for (const lettre of motAleatoire) {
-    espaceJeu.innerHTML += '<button class="btn btn-dark"></button>'
+
+for (let i = 0; i < motAleatoire.length; i++) {
+    espaceJeu.innerHTML += `<button id="${i}" class="btn btn-dark"></button>`;
+}
+
+function jouer() {
+    let lettre = document.getElementById('lettre').value;
+
+    if (motAleatoire.includes(lettre)) {
+        for (let i = 0; i < motAleatoire.length; i++) {
+            if (motAleatoire[i] === lettre) {
+                document.getElementById(i).innerText = lettre;
+
+                document.getElementById(i).classList.remove('btn-dark');
+                document.getElementById(i).classList.add('btn-primary');
+            }
+        }
+    } else {
+        if (!lettresSaisies.includes(lettre.toUpperCase())) {
+            lettresSaisies.push(lettre.toUpperCase());
+        }
+    }
+
+    document.getElementById('lettresSaisies').innerText = 'Lettres saisies : ' + lettresSaisies.join(' | ');
+    console.log(lettresSaisies);
 }
